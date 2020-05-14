3434f7c08ad45521d026e575459c6b2c3c670b25
dima-kudinov <dima-kudinov@mail.ru>
2020-05-14 13:18:36 +0400

    Gthtltkfk

--- OSlab2/Main.cs
+++ OSlab2/Main.cs
@@ -16,11 +16,6 @@ namespace OSlab2
         {
             createProcess();
             getInfo();
-            Implement();
-        }
-
-        private static void Implement()
-        {
             if (quant < 1)
             {
                 Console.WriteLine("Квант времени меньше 1");
@@ -34,7 +29,36 @@ namespace OSlab2
                     {
                         if (!Processes[i].isEmpty())
                         {
-                            implementThreads(Processes[i]);
+                            if (!(Processes[i].maxTime > 0))
+                            {
+                                Console.WriteLine("Выделенный квант времени меньше 1");
+                                Environment.Exit(0);
+                            }
+                            Console.WriteLine("\n" + Processes[i].name + "  Время макс: " + Processes[i].maxTime);
+                            int initialThreadsCount = Processes[i].Threads.Count();
+                            int currentThreadsCount = Processes[i].Threads.Count();
+
+                            for (int curThreadNum = 0; curThreadNum < currentThreadsCount;)
+                            {
+                                Thread thread = Processes[i].Threads[curThreadNum];
+                                while (thread.needTime())
+                                {
+                                    if (thread.haveTime())
+                                    {
+                                        thread.execute();
+                                        Processes[i].currentTime++;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("Максимальное время " +
+                                            "Поток " + (initialThreadsCount - currentThreadsCount) + " истекло");
+                                        break;
+                                    }
+                                }
+                                Console.WriteLine(thread.name + " завершен");
+                                Processes[i].Threads.RemoveAt(curThreadNum);
+                                currentThreadsCount--;
+                            }
                         }
                         else
                         {
@@ -52,6 +76,7 @@ namespace OSlab2
             }
             Console.WriteLine("Все процессы выполнены");
         }
+
 
         private static void getInfo()
         {
@@ -71,40 +96,6 @@ namespace OSlab2
                 Priority priority = (Priority)rand.Next(3);
                 Processes.Add(new Process("" + i, quant * (int)priority, priority));
             }
-        }
-
-        private static void implementThreads(Process process)
-        {
-            if (!(process.maxTime > 0))
-            {
-                Console.WriteLine("Выделенный квант времени меньше 1");
-                Environment.Exit(0);
-            }
-            Console.WriteLine("\n" + process.name + "  Время макс: " + process.maxTime);
-            int initialThreadsCount = process.Threads.Count();
-            int currentThreadsCount = process.Threads.Count();
-
-            for (int curThreadNum = 0; curThreadNum < currentThreadsCount;)
-            {
-                Thread thread = process.Threads[curThreadNum];
-                while (thread.needTime())
-                {
-                    if (thread.haveTime())
-                    {
-                        thread.execute();
-                        process.currentTime++;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Максимальное время " + 
-                            "Поток " + (initialThreadsCount - currentThreadsCount) + " истекло");				
-                        break;
-                    }
-                }
-                Console.WriteLine(thread.name + " завершен");
-                process.Threads.RemoveAt(curThreadNum);
-                currentThreadsCount--;
-            }
-        }
+        }        
     }
 }
